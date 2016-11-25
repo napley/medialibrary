a4d477bbbf4c1219da17ae8abfda9043b381bd1f
napley <napleyg@gmail.com>
2016-11-25 19:56:37 +0100

    mise en page de la home avec les derniers articles

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -13,7 +13,17 @@ class DefaultController extends Controller
      */
     public function indexAction(Request $request)
     {
+        $em = $this->getDoctrine()->getManager();
+
+        $books = $em->getRepository('AppBundle:Book')->findBy(array(), array('publishedDate' => 'DESC'),5);
+        $dvds = $em->getRepository('AppBundle:Dvd')->findBy(array(), array('releaseDate' => 'DESC'),5);
+        $blurays = $em->getRepository('AppBundle:Bluray')->findBy(array(), array('releaseDate' => 'DESC'),5);
+        
         // replace this example code with whatever you need
-        return $this->render('default/index.html.twig');
+        return $this->render('default/index.html.twig', [
+                    'books' => $books,
+                    'dvds' => $dvds,
+                    'blurays' => $blurays
+        ]);
     }
 }
